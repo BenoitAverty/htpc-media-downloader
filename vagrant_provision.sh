3e3f4d9f716363983d2a5f28f688cfb9cf3be8bd
Benoit Averty <benoit.averty@gmail.com>
2016-06-30 23:08:34 +0200

    Use more linuxserver images, better apache config, sabnzbd and sonar working

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 # Install docker
-apt-get update -y
-apt-get install -y apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
-apt-get update -y
-apt-get install -y linux-image-extra-$(uname -r)
-apt-get install -y docker-engine
+curl -O- https://get.docker.com | sh
+usermod -aG docker vagrant
 service docker start
 
 # Install docker-compose
